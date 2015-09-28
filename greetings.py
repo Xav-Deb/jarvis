9dcc4ca1b62ed6b5d594f4e75c873ebd2e072cb3
Xav-Deb <xavier@debonte.fr>
2015-09-29 00:20:41 +0200

    Gestion de la langue dans la date

--- greetings.py
+++ greetings.py
@@ -3,11 +3,12 @@ import sys
 import gmail
 import weather
 import time
-import locale
 
-locale.setlocale(locale.LC_ALL,'fr_FR.UTF-8')
-
-lang = "fr"
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL,'fr_FR')
+except:
+    locale.setlocale(locale.LC_ALL,'')
 
 print 'Bonjour Monsieur.'
 print 'Nous sommes le ' + time.strftime ("%A %d %B %Y") #%s %s %s.' % (time.strftime("%A"),time.strftime("%d"),time.strftime("%B"),time.strftime("%Y"))
