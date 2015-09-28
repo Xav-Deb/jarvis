0f06a46e42f52417d7ae120769122449a9226a16
Xav-Deb <xavier@debonte.fr>
2015-09-28 23:40:54 +0200

    Modif date en francais

--- greetings.py
+++ greetings.py
@@ -3,11 +3,14 @@ import sys
 import gmail
 import weather
 import time
+import locale
+
+locale.setlocale(locale.LC_ALL,'')
 
 lang = "fr"
 
 print 'Bonjour Monsieur.'
-print 'Nous sommes le %s %s %s.' % (time.strftime("%d"),time.strftime("%m"),time.strftime("%y"))
+print 'Nous sommes le ' + time.strftime ("%A %d %B %Y") #%s %s %s.' % (time.strftime("%A"),time.strftime("%d"),time.strftime("%B"),time.strftime("%Y"))
 print 'Il est %s heure %s.' % (time.strftime("%H"),time.strftime("%M"))
 print weather.main()
 print gmail.main()
